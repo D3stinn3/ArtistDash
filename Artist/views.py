1d710ead65213b90b68de24ce220319713e4404a
D3stinn3 <paynedestinne@gmail.com>
2023-07-31 21:41:27 +0300

    Final

--- Artist/views.py
+++ Artist/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def landingpage(request):
@@ -11,6 +11,14 @@ def homepage(request):
     return render()
 
 def loginpage(request):
+    if request.method == "POST":
+        username = authenticate(
+            email = request.POST['email'],
+            password = request.POST['password']
+        )
+        if username is not None:
+            login(request, username)
+            return redirect('profile')
     context = {}
     return render(request, 'temp/login.html', context)
 
